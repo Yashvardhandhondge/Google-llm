ddf5ce3ea9feef3ca54a20b90f754dc6c3e15e87
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-12-09 21:11:30 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import App from './App.tsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { Toaster } from 'react-hot-toast';
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 
 const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 // const measurementId = import.meta.env.VITE_API_MESUREMENT_ID;
